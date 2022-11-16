6b3db90b0e9df03279ffc4e9cfbc9e06aef89ad6
henry.shih <siezlh4405@outlook.com>
2022-11-17 01:59:14 +0800

    feat: 完成首頁選擇畫面 & RWD

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ServiceSelectComponent } from './components/service-select/service-select.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ServiceSelectComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
